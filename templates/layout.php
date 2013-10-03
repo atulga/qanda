a79806d51fd860b7cb6078590b0c8a92d3459f53
Ariunbayar Baterdene <abaterd@odesk.com>
2013-10-03 13:42:12 +0800

    I11: Minor coding indent fix

--- templates/layout.php
+++ templates/layout.php
@@ -25,27 +25,24 @@
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li><a href="/qanda/index.php">Нүүр хуудас</a></li>
-             <li><a href="question_add">Асуулт оруулах</a></li>
-            <?php if(logged_in()){ ?>
-                    <li><a href="profile?user_id=<?php echo $_SESSION['id'] ?> "><?php echo $_SESSION['name'] ; ?></a></li>
-                    <li><a href="logout">Гарах</a></li>
-                <?php } else {?>
-                    <li><a href="login">Нэвтрэх</a></li>
-                <?php }?>
+            <li><a href="question_add">Асуулт оруулах</a></li>
+            <?php if (logged_in()){ ?>
+                <li><a href="profile?user_id=<?php echo $_SESSION['id'] ?> "><?php echo $_SESSION['name'] ; ?></a></li>
+                <li><a href="logout">Гарах</a></li>
+            <?php } else {?>
+                <li><a href="login">Нэвтрэх</a></li>
+            <?php }?>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </div>
     
-     <div class="container">
+    <div class="container">
         <?php if (has_get('message')) {?>
-    <div class="alert alert-success">
-        <?php echo get_param('message'); ?>
-    </div>
-    <?php } ?>
-            <?php echo $content ?>
+            <div class="alert alert-success"><?php echo get_param('message'); ?></div>
+        <?php } ?>
+        <?php echo $content ?>
       </div>
-
     </div><!-- /.container -->
 
     <script src="../bootstrap/assets/js/jquery.js"></script>
