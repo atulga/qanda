49228bde61fba26dfda13d19abf5cb18e4072d6d
atulga <atu.wow@gmail.com>
2013-10-04 17:11:11 +0800

    I13:QandaHomeBundle create, show question list

--- symfony2/src/Qanda/HomeBundle/Controller/DefaultController.php
+++ symfony2/src/Qanda/HomeBundle/Controller/DefaultController.php
@@ -4,10 +4,34 @@ namespace Qanda\HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+
+use Qanda\HomeBundle\Entity\Question;
+
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/", name="/qanda/index.php/show")
+     * @Template()
+     */
+    public function indexAction()
     {
-        return $this->render('QandaHomeBundle:Default:index.html.twig', array('name' => $name));
+        $questions = $this->getDoctrine()
+            ->getRepository('QandaHomeBundle:Question')
+            ->findAll();
+/*        if (has_get('page')) {
+            $current_page = get_param('page');
+        } else {
+            $current_page = 1;
+        }
+ */
+/*      $pager = new Paginator('Question', $current_page);
+        $questions = $pager->fetch();
+ */
+        return array('questions' => $questions );
     }
+
+
 }
--- symfony2/src/Qanda/HomeBundle/Entity/Question.php
+++ symfony2/src/Qanda/HomeBundle/Entity/Question.php
@@ -2,33 +2,272 @@
 namespace Qanda\HomeBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Query;
 
 /**
- * @ORM\Entity
+ * Asuult
+ *
  * @ORM\Table(name="asuult")
+ * @ORM\Entity
  */
 class Question
 {
+    static public $_table = 'asuult';
+
     /**
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    protected $id;
+    private $id;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(name="title", type="text", nullable=false)
      */
-    protected $name;
+    private $title;
 
     /**
-     * @ORM\Column(type="decimal", scale=2)
+     * @ORM\Column(name="created_date", type="datetime", nullable=false)
      */
-    protected $price;
+    private $createdDate;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(name="question", type="text", nullable=false)
      */
-    protected $description;
+    private $question;
+
+    /**
+     * @ORM\Column(name="best_answer_id", type="integer", nullable=false)
+     */
+    private $bestAnswerId = 0;
+
+    /**
+     * @ORM\Column(name="answer_count", type="integer", nullable=false)
+     */
+    private $answerCount = 0;
+
+    /**
+     * @ORM\Column(name="user_id", type="integer", nullable=false)
+     */
+    private $userId;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Asuult
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set createdDate
+     *
+     * @param \DateTime $createdDate
+     * @return Asuult
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createdDate
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * Set question
+     *
+     * @param string $question
+     * @return Asuult
+     */
+    public function setQuestion($question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * Set bestAnswerId
+     *
+     * @param integer $bestAnswerId
+     * @return Asuult
+     */
+    public function setBestAnswerId($bestAnswerId)
+    {
+        $this->bestAnswerId = $bestAnswerId;
+
+        return $this;
+    }
+
+    /**
+     * Get bestAnswerId
+     *
+     * @return integer
+     */
+    public function getBestAnswerId()
+    {
+        return $this->bestAnswerId;
+    }
+
+    /**
+     * Set answerCount
+     *
+     * @param integer $answerCount
+     * @return Asuult
+     */
+    public function setAnswerCount($answerCount)
+    {
+        $this->answerCount = $answerCount;
+
+        return $this;
+    }
+
+    /**
+     * Get answerCount
+     *
+     * @return integer
+     */
+    public function getAnswerCount()
+    {
+        return $this->answerCount;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Asuult
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    public function isAnswered()
+    {
+        return $this->getBestAnswerId() > 0;
+    }
+
+    static public function getById($id, $as_array=false)
+    {
+        global $em;
+        $qb = $em->createQueryBuilder();
+        $query = $qb->select('a')
+            ->from('Question', 'a')
+            ->where('a.id = :id')
+            ->setParameter('id', $id)
+            ->getQuery();
+        if ($as_array){
+            $q = $query->getSingleResult(Query::HYDRATE_ARRAY);
+        }else{
+            $q = $query->getSingleResult();
+        }
+        return $q;
+    }
+
+    static public function getQuestionCount()
+    {
+        global $em;
+        $filter = array();
+        $result = $em->getRepository('Question')
+            ->findBy($filter);
+        $count = count($result);
+        return $count;
+    }
+
+    static public function getQuestions($page)
+    {
+        global $em;
+        $filter = array();
+        $order = array('createdDate' => 'DESC');
+        $one_page_rows = 5;
+        $offset = ($page -1) * $one_page_rows;
+        $questions = $em->getRepository('Question')
+            ->findBy($filter, $order, $one_page_rows, $offset);
+        return $questions;
+    }
+
+    public function getAnswers()
+    {
+        return Answer::getByQuestionId($this->getId());
+    }
+
+    static public function getLastFiveQuestionsByUserId($user_id)
+    {
+        global $em;
+        $filter = array('userId' => $user_id);
+        $order = array('createdDate' => 'DESC');
+        $questions = $em->getRepository('Question')
+            ->findBy($filter, $order, 5);
+        return $questions;
+    }
+
+    static public function getQuestionCountByUserId($user_id){
+        global $em;
+        $filter = array('userId' => $user_id);
+        $result = $em->getRepository('Question')
+            ->findBy($filter);
+        $question_count = count($result);
+        return $question_count;
+    }
 }
+
 ?>
